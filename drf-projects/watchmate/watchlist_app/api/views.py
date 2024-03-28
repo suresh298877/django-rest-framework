d37e603ed0c3dce2dc35790fbe822dcdc89043ac
suresh298877 <sureshpeddimsetti298@gmail.com>
2024-03-28 22:03:05 +0530

    adskfl

--- drf-projects/watchmate/watchlist_app/api/views.py
+++ drf-projects/watchmate/watchlist_app/api/views.py
@@ -2,6 +2,7 @@ from watchlist_app.models import Movie
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import MovieSerializer
+from rest_framework import status
 
 
 @api_view(['GET', 'POST'])
@@ -19,8 +20,19 @@ def movie_list(request):
             return Response(serializer.errors)
 
 
-@api_view()
+@api_view(['DELETE', 'PUT', 'GET'])
 def movie_details(request, pk):
     movie = Movie.objects.get(id=pk)
-    serializer = MovieSerializer(movie)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+    elif request.method == 'DELETE':
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
