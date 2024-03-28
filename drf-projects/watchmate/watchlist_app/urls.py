321801bb906102c1f40059d01cac092a88a201b3
suresh298877 <sureshpeddimsetti298@gmail.com>
2024-03-28 10:35:04 +0530

    understood the importance of django rest framework

---
+++ drf-projects/watchmate/watchlist_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from watchlist_app import views
+
+
+urlpatterns = [
+    path('list/', views.movie_list, name='movie-list'),
+    path('<int:pk>/', views.movie_detail, name="movie-detail"),
+]
